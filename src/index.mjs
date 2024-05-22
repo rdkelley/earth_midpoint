a54315143aa104b5a5052cb138e30452f9a5555d
Ryan Kelley <kelleyr@eastonrye.com>
2024-05-21 21:54:25 -0700

    Add intr point calc

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,19 @@ const convertDegreesToRadians = (deg) => (deg * Math.PI) / 180;
 
 const convertRadiansToDegrees = (rad) => rad * (180 / Math.PI);
 
+/**
+ * Parses command-line arguments and extracts latitude and longitude coordinates,
+ * along with an optional number of intermediary points.
+ *
+ * @returns {Object} An object containing the parsed values:
+ *   - `latitude_a` (number): Latitude of the first location.
+ *   - `longitude_a` (number): Longitude of the first location.
+ *   - `latitude_b` (number): Latitude of the second location.
+ *   - `longitude_b` (number): Longitude of the second location.
+ *   - `n` (number|null): The number of intermediary points, or null if not specified.
+ *
+ * @throws {Error} Throws an error if any of the latitude or longitude arguments are missing.
+ */
 const parseArgs = () => {
   const args = process.argv.slice(2);
   const values = {};
@@ -37,6 +50,7 @@ const parseArgs = () => {
   values.latitude_b = parseFloat(values.latitude_b);
   values.longitude_b = parseFloat(values.longitude_b);
 
+  console.log('\x1b[35m%s\x1b[0m', 'Data Entered:');
   console.log(
     `Location A (Lat/Long): (${values.latitude_a}, ${values.longitude_a})\nLocation B (Lat/Long): (${values.latitude_b}, ${values.longitude_b})`
   );
@@ -48,6 +62,14 @@ const parseArgs = () => {
   return values;
 };
 
+/**
+ * Converts Cartesian coordinates to latitude and longitude in radians.
+ *
+ * @param {number[]} coords - An array of Cartesian coords [x, y, z].
+ * @returns {number[]} An array:
+ *   - [0]: Latitude in radians.
+ *   - [1]: Longitude in radians.
+ */
 const coordsToLatLong = (coords) => {
   return [
     Math.atan2(coords[2], Math.sqrt(coords[0] ** 2 + coords[1] ** 2)),
@@ -55,7 +77,14 @@ const coordsToLatLong = (coords) => {
   ];
 };
 
-// Spherical interpolation
+/**
+ * Calculates the Cartesian coords of an intermediate point represented by the fraction f
+ *
+ * @param {number} f - The fraction of the distance between the two points (0 <= f <= 1).
+ * @param {Object} coords - An object containing the latitude and longitude of the two points.
+ * @returns {number[]} An array containing the Cartesian coordinates [x, y, z] of the intermediate point.
+ *
+ */
 const calculateIntermediatePoint = (f, coords, central_angle) => {
   const a = Math.sin((1 - f) * central_angle) / Math.sin(central_angle);
   const b = Math.sin(f * central_angle) / Math.sin(central_angle);
@@ -73,8 +102,14 @@ const calculateIntermediatePoint = (f, coords, central_angle) => {
   return [x, y, z];
 };
 
+/**
+ * Calculates the central angle between two geographical points on a sphere given their latitudes and longitudes.
+ *
+ * @param {Object} coords - An object containing the latitude and longitude of the two points.
+ * @returns {number} The central angle between the two points in radians.
+ *
+ */
 const calculateCentralAngle = (coords) => {
-  console.log(coords);
   const long_diff = coords.longitude_b - coords.longitude_a;
 
   const acos_paren =
@@ -86,34 +121,97 @@ const calculateCentralAngle = (coords) => {
   return Math.acos(acos_paren);
 };
 
-const arg_values = parseArgs();
-const { n, ...location_values } = arg_values;
+/**
+ * Generates an array of intermediate points along a great circle between two geographical points. Skips
+ * when the step == 0 and when step == 1 to provde n points between start and end.
+ *
+ * @param {number} n - The number of intermediate points to generate.
+ * @param {...any} calc_params - The parameters to be passed to the `calculateIntermediatePoint` function.
+ * @returns {number[][]} An array of arrays, each containing the Cartesian coordinates [x, y, z] of an intermediate point.
+ *
+ */
+const interpolate = (n, ...calc_params) => {
+  const f_steps = [];
+  const step_size = 1 / (n + 1);
 
-// Convert the values of location_values to radians
-const radian_location_values = {};
+  for (let i = 1; i <= n; i++) {
+    f_steps.push(i * step_size);
+  }
 
-Object.entries(location_values).forEach(([key, val]) => {
-  radian_location_values[key] = convertDegreesToRadians(val);
-});
+  return f_steps.map((f) => calculateIntermediatePoint(f, ...calc_params));
+};
 
-const central_angle = calculateCentralAngle(radian_location_values);
+const printResults = (mid_point, n, intr_points) => {
+  console.log(
+    '\x1b[35m%s\x1b[0m',
+    `The midpoint is located at ${mid_point[0].toFixed(
+      4
+    )} latitude, ${mid_point[1].toFixed(4)} longitude.\n`
+  );
 
-console.log('CENTRAL ANGLE', central_angle);
+  if (n) {
+    console.log(
+      '\x1b[35m%s\x1b[0m',
+      `${n} intermediary points were calculated:\n`
+    );
 
-console.log('DISTANCE', EARTH_RADIUS * central_angle);
+    for (let i = 0; i < n; i++) {
+      console.log(
+        '\x1b[35m%s\x1b[0m',
+        `${i + 1}: ${intr_points[i][0].toFixed(4)}, ${intr_points[i][1].toFixed(4)}`
+      );
+    }
+  }
+};
 
-const midpoint_coords = calculateIntermediatePoint(
-  0.5,
-  radian_location_values,
-  central_angle
-);
+const init = () => {
+  const arg_values = parseArgs();
+  let intr_points;
 
-console.log('midpoint_coords', midpoint_coords);
+  const { n, ...location_values } = arg_values;
 
-const midpoint_rads = coordsToLatLong(midpoint_coords);
+  // Convert the values of location_values to radians
+  const radian_location_values = {};
 
-console.log(
-  '\x1b[35m%s\x1b[0m',
-  midpoint_rads.map((point) => convertRadiansToDegrees(point))
-);
+  Object.entries(location_values).forEach(([key, val]) => {
+    radian_location_values[key] = convertDegreesToRadians(val);
+  });
 
+  const central_angle = calculateCentralAngle(radian_location_values);
+
+  const midpoint_coords = calculateIntermediatePoint(
+    0.5,
+    radian_location_values,
+    central_angle
+  );
+
+  const midpoint_rads = coordsToLatLong(midpoint_coords);
+
+  const mid_point = midpoint_rads.map((point) =>
+    convertRadiansToDegrees(point)
+  );
+
+  // n intermediary points requested
+  // Ignore 1 point, since that will just be the midpoint
+  if (n && n > 1) {
+    // intr_points will be array of n points, each represented by Cartesian coordinates [x, y, z]
+    const intr_points_cart = interpolate(
+      n,
+      radian_location_values,
+      central_angle
+    );
+
+    // Convert to lat/long radians... then degrees
+    intr_points = intr_points_cart.map((point_cart) =>
+      coordsToLatLong(point_cart).map((point_rads) =>
+        convertRadiansToDegrees(point_rads)
+      )
+    );
+  }
+
+  printResults(mid_point, n, intr_points);
+};
+
+init();
+
+// console.log('\x1b[35m%s\x1b[0m');
